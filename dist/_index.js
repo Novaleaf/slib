12e5b4055e8e45befdf76a54a727604f02177050
Jason <JasonS@Novaleaf.com>
2016-09-30 19:07:14 -0700

    scafolding stub

---
+++ dist/_index.js
@@ -0,0 +1,7 @@
+"use strict";
+var xlib = require("xlib");
+var log = new xlib.logging.Logger(__filename);
+log.info("hi thar from slib");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = "hi";
+//# sourceMappingURL=_index.js.map
---
+++ src/_index.ts
@@ -0,0 +1,9 @@
+﻿import * as xlib from "xlib";
+
+
+let log = new xlib.logging.Logger(__filename);
+
+log.info("hi thar from slib");
+
+
+export default "hi";
