633af5a75dddd35db85ae15aba377e66e0818d13
Jason <JasonS@Novaleaf.com>
2019-02-28 20:20:34 -0800

    add re2 for safe regexp

--- src/_index.ts
+++ src/_index.ts
@@ -3,6 +3,9 @@ export { xlib };
 import * as io from "./io";
 export { io };
 
+/** re2 is a safe, linear time regexp implementation.  https://www.npmjs.com/package/re2  */
+export const re2: typeof RegExp = require( "re2" );
+
 // import glob = require( "glob-promise" );
 
 // glob("")
