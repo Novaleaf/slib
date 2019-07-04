8a44531d0b4692c1af2e8d649cc395de1cce1955
Jason <JasonS@Novaleaf.com>
2019-07-04 13:48:43 -0700

    upgrade submodules and add cpuUsage helper method

--- src/node.ts
+++ src/node.ts
@@ -1,1 +1,12 @@
 export import os = require( "os" );
+
+
+let cpuCount = os.cpus().length;
+
+export function getCpuUsagePercent() {
+	try {
+		return os.loadavg()[ 0 ] / cpuCount;
+	} catch{
+		return Number.NaN;
+	}
+}
